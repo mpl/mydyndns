b3cc9c1658ac63ffd22a449b42368362273e8605
mpl <mathieu.lonjaret@gmail.com>
2018-05-21 04:42:06 +0200

    updating ip works

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -21,7 +23,7 @@ func main() {
 		log.Fatal(err)
 	}
 	defer f.Close()
-	data, err := updateDB(f)
+	data, err := updateDB(f, "home", "1.2.3.4")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,59 +37,79 @@ const simpleRFC3339 = "20060102"
 // var updateStampRgx = regexp.MustCompile(`(\s*)(\d{8})(.*);`)
 var updateStampRgx = regexp.MustCompile(`(\s*)(\d{8})(\d+);`)
 
-func updateDB(r io.Reader) ([]byte, error) {
+func updateDB(r io.Reader, name, ip string) ([]byte, error) {
+	if IP := net.ParseIP(ip); IP == nil {
+		return nil, fmt.Errorf("not a valid IP: %q", ip)
+	}
+	// TODO(mpl): revert timestamp/everything if actual update does not work. Probably simpler: just create new temp file and overwrite real one if everything went fine.
 	var buf bytes.Buffer
 	sc := bufio.NewScanner(r)
-	dateDone := false
+	dateDone, ipDone := false, false
 	for sc.Scan() {
 		l := sc.Text()
-		if dateDone {
-			if _, err := buf.Write([]byte(l+"\n")); err != nil {
+		if dateDone && ipDone {
+			if _, err := buf.Write([]byte(l + "\n")); err != nil {
 				return nil, err
 			}
 			continue
 		}
 
-			m := updateStampRgx.FindStringSubmatch(l)
-			if m != nil {
-				println(l, "MATCHED")
-			}
-			if m == nil || len(m) != 4 {
-				if _, err := buf.Write([]byte(l+"\n")); err != nil {
+		if dateDone {
+			fields := strings.Fields(l)
+			if len(fields) != 4 || fields[1] != "IN" || fields[2] != "A" || fields[0] != name {
+				if _, err := buf.Write([]byte(l + "\n")); err != nil {
 					return nil, err
 				}
 				continue
 			}
-			datePart := m[2]
-			if _, err := time.Parse(simpleRFC3339, datePart); err != nil {
-				log.Printf("%v", err)
-				if _, err := buf.Write([]byte(l+"\n")); err != nil {
-					return nil, err
-				}
-				continue
-			}
-			i, err := strconv.Atoi(m[3])
-			if err != nil {
-				log.Printf("%v", err)
-				if _, err := buf.Write([]byte(l+"\n")); err != nil {
-					return nil, err
-				}
-				continue
-			}
-			whiteSpace := m[1]
-			var updateStamp string
-			today := time.Now().Format(simpleRFC3339)
-			if datePart == today {
-				i++
-				updateStamp = fmt.Sprintf("%s%s%d;\n", whiteSpace, today, i)
-			} else {
-				println(today, "VS", datePart)
-				updateStamp = whiteSpace + today + "1;\n"
-			}
-			if _, err := buf.Write([]byte(updateStamp)); err != nil {
+			newValue := fmt.Sprintf("%s\tIN\tA\t%s\n", name, ip)
+			if _, err := buf.Write([]byte(newValue)); err != nil {
 				return nil, err
 			}
-			dateDone = true
+			ipDone = true
+			continue
+		}
+
+		m := updateStampRgx.FindStringSubmatch(l)
+		if m != nil {
+			println(l, "MATCHED")
+		}
+		if m == nil || len(m) != 4 {
+			if _, err := buf.Write([]byte(l + "\n")); err != nil {
+				return nil, err
+			}
+			continue
+		}
+		datePart := m[2]
+		if _, err := time.Parse(simpleRFC3339, datePart); err != nil {
+			log.Printf("%v", err)
+			if _, err := buf.Write([]byte(l + "\n")); err != nil {
+				return nil, err
+			}
+			continue
+		}
+		i, err := strconv.Atoi(m[3])
+		if err != nil {
+			log.Printf("%v", err)
+			if _, err := buf.Write([]byte(l + "\n")); err != nil {
+				return nil, err
+			}
+			continue
+		}
+		whiteSpace := m[1]
+		var updateStamp string
+		today := time.Now().Format(simpleRFC3339)
+		if datePart == today {
+			i++
+			updateStamp = fmt.Sprintf("%s%s%d;\n", whiteSpace, today, i)
+		} else {
+			println(today, "VS", datePart)
+			updateStamp = whiteSpace + today + "1;\n"
+		}
+		if _, err := buf.Write([]byte(updateStamp)); err != nil {
+			return nil, err
+		}
+		dateDone = true
 	}
 	if err := sc.Err(); err != nil {
 		return nil, err
