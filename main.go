8d3ccf771c00c6047df91175fb863c3714c73f40
mpl <mathieu.lonjaret@gmail.com>
2019-03-21 10:18:55 +0100

    authorized domains: re-add sim

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ var (
 	up                       []*basicauth.UserPass
 	// for now let's just do a direct mapping between username and domain,
 	// except for the initial user who can do anything.
-	authorizedDomains = []string{"home", "jo", "jb"}
+	authorizedDomains = []string{"home", "jo", "sim", "jb"}
 )
 
 func usage() {
